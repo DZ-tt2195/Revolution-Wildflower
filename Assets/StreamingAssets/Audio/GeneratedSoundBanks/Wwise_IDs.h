0ed2f4a65ff8c90351c330f3bc7bd598e03c8485
Kangasaur <cosmicllama42@gmail.com>
2023-11-01 18:32:26 -0400

    add existing audio into wwise

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -11,6 +11,24 @@
 
 namespace AK
 {
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_BOTTLE_PLACEHOLDER = 3382157760U;
+        static const AkUniqueID PLAY_BUTTON_BASE_PLACEHOLDER = 4193244264U;
+        static const AkUniqueID PLAY_BUTTON_BASE_PLACEHOLDER_01 = 3668726462U;
+        static const AkUniqueID PLAY_CARD_MOVE = 2342783904U;
+        static const AkUniqueID PLAY_CARD_PLAY = 3452973965U;
+        static const AkUniqueID PLAY_CHARACTERFOOTSTEPS = 2603299628U;
+        static const AkUniqueID PLAY_END_TURN_PLACEHOLDER = 2000740979U;
+        static const AkUniqueID PLAY_GUARD_KO_PLACEHOLDER = 2157469724U;
+        static const AkUniqueID PLAY_GUARD_NOTICE_PLACEHOLDER = 3136084898U;
+        static const AkUniqueID PLAY_GUNSHOT_PLACEHOLDER = 1795322234U;
+        static const AkUniqueID PLAY_MOVEMENT_SELECT_PLACEHOLDER = 3274460198U;
+        static const AkUniqueID PLAY_STATICMUSIC = 4097918453U;
+        static const AkUniqueID PLAY_UI_BUTTON_HOVER = 310530060U;
+        static const AkUniqueID STOP_OFFICEAMBIENTNOISE = 3265032750U;
+    } // namespace EVENTS
+
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
