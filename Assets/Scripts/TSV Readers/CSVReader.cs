7a6e46a33a2b218c04a38aa354ee6982ba04753c
Kangasaur <cosmicllama42@gmail.com>
2023-11-01 15:52:21 -0400

    extra comments on previous scripts

--- Assets/Scripts/TSV Readers/CSVReader.cs
+++ Assets/Scripts/TSV Readers/CSVReader.cs
@@ -8,16 +8,16 @@ using System.Text.RegularExpressions;
 
 public class TSVReader
 {
-	static string SPLIT_RE = @"\t(?=(?:[^""]*""[^""]*"")*(?![^""]*""))";
+	static string SPLIT_RE = @"\t(?=(?:[^""]*""[^""]*"")*(?![^""]*""))"; //Regular expressions to split TSVs
 	static string LINE_SPLIT_RE = @"\r\n|\n\r|\n|\r";
 	static char[] TRIM_CHARS = { '\"' };
 
 	/// <summary>
-	/// Reads a TSV from a file in the Resources folder and outputs a jagged array of strings
+	/// Reads a TSV from a file in the Resources folder for use in card data generation
 	/// </summary>
 	/// <param name="file">The path of the file to load</param>
 	/// <param name="headerLines">The number of lines at the top to skip over as headers</param>
-	/// <returns></returns>
+	/// <returns>A jagged array of strings to use in card scripts</returns>
 	public static string[][] ReadCards(string file, int headerLines)
 	{
 		TextAsset data = Resources.Load(file) as TextAsset;
@@ -34,7 +34,12 @@ public class TSVReader
 		return list;
 	}
 
-    public static string[,] ReadLevel(string file)
+	/// <summary>
+	/// Reads a TSV from a file in the Resources folder for use in level generation
+	/// </summary>
+	/// <param name="file">The path of the file to load</param>
+	/// <returns>A 2D array of strings to be translated into the level grid</returns>
+	public static string[,] ReadLevel(string file)
     {
         TextAsset data = Resources.Load(file) as TextAsset;
         var rows = Regex.Split(data.text, LINE_SPLIT_RE);
