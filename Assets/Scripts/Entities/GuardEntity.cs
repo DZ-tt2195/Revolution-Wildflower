1eb06dba72658cc95a8136a74189a092b6bb5461
Nicole Conrad Shrader <82776089+shradercinc@users.noreply.github.com>
2023-12-13 21:56:22 -0500

    tried to upload a fix?

--- Assets/Scripts/Entities/GuardEntity.cs
+++ Assets/Scripts/Entities/GuardEntity.cs
@@ -19,6 +19,7 @@ public class GuardEntity : MovingEntity
     [Header("Detection")]
         [Tooltip("Tiles this is searching")] List<TileData> inDetection = new List<TileData>();
         [Tooltip("Pauses between movement")] float movePauseTime = 0.25f;
+        int DetectionRangeMax = 0;
         [Tooltip("How far this can see")] [SerializeField] public int DetectionRangePatrol = 3;
         [Tooltip("half their field of view for detection (MUST BE A MULTIPLE OF 5)")] [SerializeField] int DetectionAngle = 30;
         [Tooltip("State of a guard's alert")] Alert alertStatus = 0;
