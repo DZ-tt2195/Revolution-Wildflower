e8253a9e3ae69a5fcaa2e897086c796361728998
Nicole Conrad Shrader <82776089+shradercinc@users.noreply.github.com>
2024-02-26 02:02:17 -0500

    one final push before bed

--- Assets/Scripts/Entities/StaticGuard.cs
+++ Assets/Scripts/Entities/StaticGuard.cs
@@ -14,7 +14,25 @@ public class StaticGuard : GuardEntity
         {
             yield break;
         }
+        NewManager.instance.CalculatePathfinding(currentTile, NewManager.instance.listOfTiles[PatrolPoints[PatrolTarget].x, PatrolPoints[PatrolTarget].y], movementLeft, true, true);
+        TileData nextTile = NewManager.instance.CurrentAvailableMoveTarget;  //moves towards the next patrol point
+        Vector2Int nextDirection = nextTile.gridPosition - currentTile.gridPosition;
 
+        if (nextDirection != direction)
+        {
+            direction = nextDirection;
+            CalculateTiles();
+        }
+        else
+        {
+            //print("moving too " + nextTile.gridPosition);
+            if (nextTile.myEntity == null)
+            {
+                MoveTile(nextTile);//move to the tile
+                //footsteps.Post(gameObject);
+            }
+            movementLeft--;
+        }
         //return base.Patrol();
     }
 }
