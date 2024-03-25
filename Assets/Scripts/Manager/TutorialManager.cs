d0faa08ec3124f7f4bdf9d2184b72876b01a1380
Noah <dacrashbomber@gmail.com>
2024-03-25 15:09:13 -0400

    changed player bars

--- Assets/Scripts/Manager/TutorialManager.cs
+++ Assets/Scripts/Manager/TutorialManager.cs
@@ -2,7 +2,6 @@ using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
-using UnityEditor.Timeline.Actions;
 using UnityEngine;
 using UnityEngine.UI;
 
